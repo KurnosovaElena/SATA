d8a0b69c7658e2758feebfcecbe8ef10c9cb7b1e
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-04-29 21:54:54 +0300

    feat: add registration form

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/main/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./main.css"
+function MainPage() {
+  return (
+    <div>MainPage</div>
+  )
+}
+
+export default MainPage
