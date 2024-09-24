44cb941171b9cd546685e9ee8eeb8cd03d3b374f
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-09-24 18:48:36 +0300

    feat: add SubgroupsIsNullOrEmpty exception message

--- AssemblyService.BusinessLogic/Exceptions/ExceptionMessages/DepartmentExceptionMessages.cs
+++ AssemblyService.BusinessLogic/Exceptions/ExceptionMessages/DepartmentExceptionMessages.cs
@@ -7,4 +7,7 @@ public static class DepartmentExceptionMessages
 {
     public static string GroupsIsNullOrEmpty(Guid id) =>
         $"department {id} groups is null or empty";
+
+    public static string SubgroupsIsNullOrEmpty(Guid id) =>
+        $"group {id} does not have subgroups";
 }
