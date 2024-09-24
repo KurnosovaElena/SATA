166cc7e22890bc90c6fd80485fd58d9fb14699ef
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-09-24 17:50:34 +0300

    feat: add mapster

---
+++ AssemblyService.BusinessLogic/DI/ServicesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AssemblyService.DataAccess.DI;
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AssemblyService.BusinessLogic.DI;
+
+public static class ServicesConfiguration
+{
+    /// <summary>
+    /// Inject Dependencies from Business Logic Layer 
+    /// </summary>
+    /// <param name="services">Collection of services from API</param>
+    /// <param name="configuration">Configuration properties from API</param>
+    public static void AddBLLDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDataAccessDependencies(configuration);
+
+        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ AssemblyService.BusinessLogic/MappingConfigurations/GlobalMappinSettings.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+
+namespace AssemblyService.BusinessLogic.MappingConfigurations;
+
+/// <summary>
+/// Mapster Configuration
+/// </summary>
+public class GlobalMappingSettings : IRegister
+{
+    /// <summary>
+    /// Register custom mapping configurations 
+    /// </summary>
+    /// <param name="config">config argument</param>
+    public void Register(TypeAdapterConfig config)
+    {
+        TypeAdapterConfig.GlobalSettings.Default.MaxDepth(2);
+        TypeAdapterConfig.GlobalSettings.Default.PreserveReference(true);
+    }
+}
