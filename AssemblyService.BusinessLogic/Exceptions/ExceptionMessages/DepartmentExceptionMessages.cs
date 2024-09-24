6e6d411887a8bdc436bff2d061a9538168711163
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-09-24 17:57:58 +0300

    feat: add exceptions

---
+++ AssemblyService.BusinessLogic/Exceptions/ExceptionMessages/DepartmentExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace AssemblyService.BusinessLogic.Exceptions.ExceptionMessages;
+
+/// <summary>
+/// Exception messages builder
+/// </summary>
+public static class DepartmentExceptionMessages
+{
+    public static string GroupsIsNullOrEmpty(Guid id) =>
+        $"department {id} groups is null or empty";
+}
