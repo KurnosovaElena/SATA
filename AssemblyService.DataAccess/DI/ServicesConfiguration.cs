d5e2a105ac93acc8c23e5969dcb05d229f3d1d7e
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-09-24 13:28:07 +0300

    feat: add di

---
+++ AssemblyService.DataAccess/DI/ServicesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AssemblyService.DataAccess.Context;
+using AssemblyService.DataAccess.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AssemblyService.DataAccess.DI;
+public static class ServicesConfiguration
+{
+    public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TimetableContext>(options =>
+        options.UseNpgsql(
+            configuration.GetConnectionString("PostgreSQLConnection"),
+            connectionOptions => connectionOptions.EnableRetryOnFailure()
+            ).UseLazyLoadingProxies());
+
+        services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+    }
+}
